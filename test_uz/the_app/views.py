f04f45d6715f2db8b4cc1f2ea7bc57828e2bf742
curiousmf <129069615+curiousmf@users.noreply.github.com>
2023-05-17 16:34:39 +0500

    v2

--- test_uz/the_app/views.py
+++ test_uz/the_app/views.py
@@ -4,12 +4,11 @@ from the_app.models import Book
 
 # Create your views here.
 def test_func(request):
-    
+
     books = Book.objects.all()
     print(books)
 
     for book in books:
         print(book)
-    f_book = Book.objects.filter(page_count__gte(10))
               
     return render(request, 'index.html')
